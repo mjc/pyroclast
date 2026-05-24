5fbee20fe6ba9ba64b8a8ac1026d3a7248cec4c4
Mika Cohen <mjc@kernel.org>
2026-05-24 15:25:25 -0600

    Move cli output writing into module

---
+++ src/output.rs
@@ -0,0 +1,25 @@
+use std::io::{ErrorKind, Write};
+
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct CliOutput {
+    pub stdout: String,
+    pub stderr: String,
+}
+
+pub fn write_cli_output(
+    output: CliOutput,
+    mut stdout: impl Write,
+    mut stderr: impl Write,
+) -> std::io::Result<()> {
+    write_all_or_ignore_broken_pipe(&mut stdout, output.stdout.as_bytes())?;
+    write_all_or_ignore_broken_pipe(&mut stderr, output.stderr.as_bytes())?;
+    Ok(())
+}
+
+fn write_all_or_ignore_broken_pipe(writer: &mut impl Write, bytes: &[u8]) -> std::io::Result<()> {
+    match writer.write_all(bytes) {
+        Ok(()) => Ok(()),
+        Err(error) if error.kind() == ErrorKind::BrokenPipe => Ok(()),
+        Err(error) => Err(error),
+    }
+}
